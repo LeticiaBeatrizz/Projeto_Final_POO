86723807005aba98b07967a6c830bac0d410d6ca
LeticiaBeatrizz <b.freire@escolar.ifrn.edu.br>
2025-08-10 22:58:59 -0300

    Update janela_buscar.py

--- janelas/janela_buscar.py
+++ janelas/janela_buscar.py
@@ -6,24 +6,6 @@ class TelaBuscaReserva:
     def __init__(self, container):
         self.criar_widgets(container)
 
-    def criar_abas(self):
-        style = ttk.Style()
-        style.configure("TNotebook.Tab", padding=[10, 5])
-        self.notebook = ttk.Notebook(self.janela)
-        self.notebook.pack(expand=True, fill="both")
-
-        aba_reserva = ttk.Frame(self.notebook)
-        aba_buscar = ttk.Frame(self.notebook)
-        aba_cancelar = ttk.Frame(self.notebook)
-        aba_checagem = ttk.Frame(self.notebook)
-
-        self.notebook.add(aba_reserva, text="Reserva")
-        self.notebook.add(aba_buscar, text="Buscar")
-        self.notebook.add(aba_checagem, text="Checagem")
-        self.notebook.add(aba_cancelar, text="Cancelar")
-
-        self.criar_widgets(aba_buscar)
-    
     def criar_widgets (self, container,):
         self.label_nome = tk.Label(container, text="Nome do Hóspede:")
         self.label_nome.pack(pady=5)
